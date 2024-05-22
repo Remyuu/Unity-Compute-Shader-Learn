6b77873fbaea339e63611e60350bc99d75242936
Remyuu <zaomiaoe@outlook.com>
2024-05-22 14:45:49 +0800

    L2_Using_Buffer

--- Assets/Scripts/BufferJoy.cs
+++ Assets/Scripts/BufferJoy.cs
@@ -7,6 +7,13 @@ public class BufferJoy : MonoBehaviour
     public ComputeShader shader;
     public int texResolution = 1024;
 
+    struct Circle
+    {
+        public Vector2 origin;
+        public Vector2 velocity;
+        public float radius;
+    }
+    
     Renderer rend;
     RenderTexture outputTexture;
 
@@ -15,6 +22,10 @@ public class BufferJoy : MonoBehaviour
 
     public Color clearColor = new Color();
     public Color circleColor = new Color();
+    
+    Circle[] circleData;  // 在CPU上
+    
+    ComputeBuffer buffer; // 在GPU上
 
     int count = 10;
 
@@ -36,6 +47,30 @@ public class BufferJoy : MonoBehaviour
     private void InitData()
     {
         circlesHandle = shader.FindKernel("Circles");
+        
+        uint threadGroupSizeX;
+
+        shader.GetKernelThreadGroupSizes(circlesHandle, out threadGroupSizeX, out _, out _);
+
+        int total = (int)threadGroupSizeX * count;
+        circleData = new Circle[total];
+
+        float speed = 100;
+        float halfSpeed = speed * 0.5f;
+        float minRadius = 10.0f;
+        float maxRadius = 30.0f;
+        float radiusRange = maxRadius - minRadius;
+
+        for(int i=0; i<total; i++)
+        {
+            Circle circle = circleData[i];
+            circle.origin.x = Random.value * texResolution;
+            circle.origin.y = Random.value * texResolution;
+            circle.velocity.x = (Random.value * speed) - halfSpeed;
+            circle.velocity.y = (Random.value * speed) - halfSpeed;
+            circle.radius = Random.value * radiusRange + minRadius;
+            circleData[i] = circle;
+        }
     }
 
     private void InitShader()
@@ -45,6 +80,11 @@ public class BufferJoy : MonoBehaviour
         shader.SetVector( "clearColor", clearColor );
         shader.SetVector( "circleColor", circleColor );
         shader.SetInt( "texResolution", texResolution );
+        
+        int stride = (2 + 2 + 1) * 4; //2 floats origin, 2 floats velocity, 1 float radius - 4 bytes per float
+        buffer = new ComputeBuffer(circleData.Length, stride);
+        buffer.SetData(circleData);
+        shader.SetBuffer(circlesHandle, "circlesBuffer", buffer);
 		
 		shader.SetTexture( clearHandle, "Result", outputTexture );
         shader.SetTexture( circlesHandle, "Result", outputTexture );
