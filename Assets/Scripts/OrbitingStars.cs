05ed0566727354290d4dd61b049ea3094b86ed36
Remyuu <zaomiaoe@outlook.com>
2024-05-22 16:27:20 +0800

    L2_GetData

--- Assets/Scripts/OrbitingStars.cs
+++ Assets/Scripts/OrbitingStars.cs
@@ -14,12 +14,19 @@ public class OrbitingStars : MonoBehaviour
     int groupSizeX;
     
     Transform[] stars;
+    ComputeBuffer resultBuffer; // Buffer
+    Vector3[] output;			// CPU接受
     
     void Start()
     {
         kernelHandle = shader.FindKernel("OrbitingStars");
         shader.GetKernelThreadGroupSizes(kernelHandle, out threadGroupSizeX, out _, out _);
         groupSizeX = (int)((starCount + threadGroupSizeX - 1) / threadGroupSizeX);
+        
+        //buffer on the gpu in the ram
+        resultBuffer = new ComputeBuffer(starCount, sizeof(float) * 3);
+        shader.SetBuffer(kernelHandle, "Result", resultBuffer);
+        output = new Vector3[starCount];
 
         stars = new Transform[starCount];
         for (int i = 0; i < starCount; i++)
@@ -30,6 +37,16 @@ public class OrbitingStars : MonoBehaviour
 
     void Update()
     {
-        
+        shader.SetFloat("time", Time.time);
+        shader.Dispatch(kernelHandle, groupSizeX, 1, 1);
+        resultBuffer.GetData(output);
+
+        for (int i = 0; i < stars.Length; i++)
+            stars[i].localPosition = output[i];
+    }
+    
+    void OnDestroy()
+    {
+        resultBuffer.Dispose();
     }
 }
